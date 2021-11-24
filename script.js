11821b863f439ff16573e23eff869992f2ef9542
SCBalate <67940266+SCBalate@users.noreply.github.com>
2021-11-24 15:04:44 +0530

    added css and js to the html file

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function myFunction1(){
+    document.getElementById("main-doc").style.fontSize = "1.5em";
+}
+function myFunction2(){
+    document.getElementById("main-doc").style.fontSize = "1em";
+}
+function myFunction3(){
+    document.getElementById("main-doc").style.fontSize = "0.5em";
+}
